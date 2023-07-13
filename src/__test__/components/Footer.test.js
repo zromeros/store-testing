e8b9f629b8abf25a291357ce0cffe13c3af85141
zadkiel <zadkiel.romero@pagodirecto.com>
2023-07-13 17:54:29 -0400

    [ADD] last types of testing

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -9,5 +10,12 @@ describe('<Footer />', () => {
   });
   test('Render del título', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer Snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
